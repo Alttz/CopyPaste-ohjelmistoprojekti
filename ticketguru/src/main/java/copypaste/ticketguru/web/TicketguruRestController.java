8b974f2b7626b5a4e0b75168bd1f41a29f714df1
cmdtvt <tvtuusa@gmail.com>
2024-02-25 15:42:17 +0200

    Mostly working delete call

--- ticketguru/src/main/java/copypaste/ticketguru/web/TicketguruRestController.java
+++ ticketguru/src/main/java/copypaste/ticketguru/web/TicketguruRestController.java
@@ -1,10 +1,12 @@
 package copypaste.ticketguru.web;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import copypaste.ticketguru.domain.Event;
 import copypaste.ticketguru.domain.EventRepository;
 
@@ -18,6 +20,34 @@ public class TicketguruRestController {
 	public @ResponseBody List<Event> eventListRest() {
 		return (List<Event>) erepository.findAll();
 	}
+
+	//Delete
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteEvent(@PathVariable long id) {
+		if(!erepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		Event item = erepository.findById(id).get();
+		erepository.delete(item);
+		//erepository.deleteById(id);
+		return ResponseEntity.ok().build();
+	}
+
+	/*
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteEvent(@PathVariable long id) {
+		if(!erepository.existsById(id)) {
+			return new ResponseEntity<>("HELLO",HttpStatus.NO_CONTENT);
+		}
+		//Tried using responseEntity differently to get an output.
+		if(!erepository.existsById(id)) {
+			return new ResponseEntity<>("HELLO",HttpStatus.NO_CONTENT);
+		}
+		erepository.delete(erepository.findById(id).get());
+		return new ResponseEntity<>("HELLO", HttpStatus.OK);
+	}
+	*/
+
 
 
 }
