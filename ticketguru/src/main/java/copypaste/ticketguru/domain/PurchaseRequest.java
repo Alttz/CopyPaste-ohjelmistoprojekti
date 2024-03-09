f796926f15d0012d1e7b4c6d543507adcdd39808
nyDropTheC <125159296+nyDropTheC@users.noreply.github.com>
2024-03-09 23:42:40 +0200

    Implemented purchases for multiple events
    - This is dumb
    - This is stupid
    - Do NOT merge this
    - I HATE this API design
    - Needs API documentation

--- ticketguru/src/main/java/copypaste/ticketguru/domain/PurchaseRequest.java
+++ ticketguru/src/main/java/copypaste/ticketguru/domain/PurchaseRequest.java
@@ -2,10 +2,21 @@ package copypaste.ticketguru.domain;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
 public class PurchaseRequest {
+    @NotNull(message = "Event cannot be NULL")
 	private Long eventId;
-    private Long userId; 
-    private List<String> ticketTypeNames; 
+
+    @NotNull(message = "User cannot be NULL")
+    private Long userId;
+
+    @NotEmpty(message = "Tickets cannot be empty")
+    @NotNull(message = "Tickets cannot be NULL")
+    private List<@NotBlank String> ticketTypeNames;
+    // Would be good to also have validation for ticketTypes being in the valid set, but that'd require stupid code
 
     public Long getEventId() {
         return eventId;
@@ -30,8 +41,4 @@ public class PurchaseRequest {
     public void setTicketTypeNames(List<String> ticketTypeNames) {
         this.ticketTypeNames = ticketTypeNames;
     }
-
-
-	
-
 }
--- ticketguru/src/main/java/copypaste/ticketguru/web/rest/PurchaseRestController.java
+++ ticketguru/src/main/java/copypaste/ticketguru/web/rest/PurchaseRestController.java
@@ -1,13 +1,20 @@
 package copypaste.ticketguru.web.rest;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.atomic.LongAccumulator;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +31,8 @@ import copypaste.ticketguru.domain.PurchaseRequest;
 import copypaste.ticketguru.domain.TicketRepository;
 import copypaste.ticketguru.domain.TicketTypeRepository;
 import copypaste.ticketguru.domain.UserRepository;
+import io.micrometer.common.lang.NonNull;
+import jakarta.validation.Valid;
 
 @RestController
 public class PurchaseRestController {
@@ -50,52 +59,103 @@ public class PurchaseRestController {
         return (List<Purchase>) purchaseRepository.findAll();
     }
 
+    
+
     // Luodaan ostotapahtuma. Toistaiseksi ei pysty käyttämään useampaan
     @PostMapping("/api/purchases")
-    public ResponseEntity<?> createPurchaseWithTickets(@RequestBody PurchaseRequest purchaseRequest) {
-        // Tarkastetaan löytyykö tapahtuma id:n perusteella
-        return eventRepository.findById(purchaseRequest.getEventId()).map(event -> {
-            // tarkatetaan, että lippumäärä on riittävä
-            if (event.getTicketCount() < purchaseRequest.getTicketTypeNames().size()) {
-                return ResponseEntity.badRequest().body("Tapahtumaan ei ole riittävästi lippuja vapaana.");
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> createPurchaseWithTickets(@Valid @RequestBody List<PurchaseRequest> purchaseRequestList) {
+        // This is a very STUPID IDEA AND SHOULD NOT BE MERGED FOR THE LOVE OF GOD
+        // Please forgive me for this monstrosity that breaks every single convention of API design
+        class PurchaseRequestResponse {
+            private boolean isSuccessful;
+            private String errorMessage;
+    
+            public boolean isSuccessful() {
+                return isSuccessful;
+            }
+            public void setSuccessful(boolean isSuccessful) {
+                this.isSuccessful = isSuccessful;
+            }
+            public String getErrorMessage() {
+                return errorMessage;
+            }
+            public void setErrorMessage(String errorMessage) {
+                this.errorMessage = errorMessage;
+            }
+            public PurchaseRequestResponse(boolean isSuccessful, String errorMessage) {
+                this.isSuccessful = isSuccessful;
+                this.errorMessage = errorMessage;
+            }
+            public PurchaseRequestResponse() {
+                this.isSuccessful = false;
+                this.errorMessage = "";
+            }
+        }
+
+        HashMap<Long, PurchaseRequestResponse> responseMap = new HashMap<>();
+
+        boolean atLeastOneWasSuccessful = false;
+
+        for(PurchaseRequest purchaseRequest : purchaseRequestList) {
+            PurchaseRequestResponse resp = eventRepository.findById(purchaseRequest.getEventId()).map(event -> {
+                PurchaseRequestResponse status = new PurchaseRequestResponse();
+
+                if(event.getTicketCount() < purchaseRequest.getTicketTypeNames().size()) {
+                    status.setErrorMessage("Tapahtumaan ei ole riittävästi lippuja vapaana.");
+                    return status;
+                }
+
+                Optional<AppUser> userOpt = userRepository.findById(purchaseRequest.getUserId());
+                if (!userOpt.isPresent()) {
+                    status.setErrorMessage("Käyttäjää ei löytynyt");
+                    return status;
+                }
+                AppUser appUser = userOpt.get();
+
+                List<TicketType> ticketTypes = ticketTypeRepository.findByEventAndNameIn(event,
+                purchaseRequest.getTicketTypeNames());
+
+                // tarkastetaan lipputyypin nimen perusteella löytyykö määritetyt lipputyypit
+                // tapahtuman lipputyypeistä
+                if (ticketTypes.size() != purchaseRequest.getTicketTypeNames().size()) {
+                    status.setErrorMessage("Yhtä tai useampaa määritettyä lipputyyppiä ei löydy tälle tapahtumalle.");
+                    return status;
+                }
+
+                List<Ticket> tickets = ticketTypes.stream().map(tt -> new Ticket(tt, event, null, false))
+                        .collect(Collectors.toList());
+
+                // Päivitä tapahtuman lippumäärä
+                event.setTicketCount(event.getTicketCount() - tickets.size());
+                eventRepository.save(event);
+
+                // tallenna ostotapahtuma
+                Purchase purchase = new Purchase(new Date(), tickets, appUser);
+                Purchase savedPurchase = purchaseRepository.save(purchase);
+
+                // tallenna kullekin lipulle ostotapahtuma, johon ne kuuluu
+                tickets.forEach(ticket -> {
+                    ticket.setPurchase(savedPurchase);
+                    ticketRepository.save(ticket);
+                });
+
+                status.setSuccessful(true);
+
+                return status;
+            }).orElse(new PurchaseRequestResponse(false, "Tapahtumaa ei löytynyt"));
+
+            if(!atLeastOneWasSuccessful) {
+                atLeastOneWasSuccessful = resp.isSuccessful();
             }
 
-            // Tarkastetaan löytyykö käyttäjä id:n perusteella
-            Optional<AppUser> userOpt = userRepository.findById(purchaseRequest.getUserId());
-            if (!userOpt.isPresent()) {
-                return ResponseEntity.notFound().build();
-            }
-            AppUser appUser = userOpt.get();
+            responseMap.put(purchaseRequest.getEventId(), resp);
+        }
 
-            // Haetaan lipputyypit tapahtuman ja nimen mukaan, jolloin pystytään
-            // toimittamaan vain lipputyyppien-nimet ostotapahtuman yhteydessä
-            List<TicketType> ticketTypes = ticketTypeRepository.findByEventAndNameIn(event,
-                    purchaseRequest.getTicketTypeNames());
+        if(!atLeastOneWasSuccessful) {
+            return ResponseEntity.badRequest().body(responseMap);
+        }
 
-            // tarkastetaan lipputyypin nimen perusteella löytyykö määritetyt lipputyypit
-            // tapahtuman lipputyypeistä
-            if (ticketTypes.size() != purchaseRequest.getTicketTypeNames().size()) {
-                return ResponseEntity.badRequest().body("Yhtä tai useampaa määritettyä lipputyyppiä ei löydy tälle tapahtumalle.");
-            }
-
-            List<Ticket> tickets = ticketTypes.stream().map(tt -> new Ticket(tt, event, null, false))
-                    .collect(Collectors.toList());
-
-            // Päivitä tapahtuman lippumäärä
-            event.setTicketCount(event.getTicketCount() - tickets.size());
-            eventRepository.save(event);
-
-            // tallenna ostotapahtuma
-            Purchase purchase = new Purchase(new Date(), tickets, appUser);
-            Purchase savedPurchase = purchaseRepository.save(purchase);
-
-            // tallenna kullekin lipulle ostotapahtuma, johon ne kuuluu
-            tickets.forEach(ticket -> {
-                ticket.setPurchase(savedPurchase);
-                ticketRepository.save(ticket);
-            });
-
-            return ResponseEntity.status(HttpStatus.CREATED).body(savedPurchase);
-        }).orElse(ResponseEntity.notFound().build());
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseMap);
     }
 }
