d1af58a5ebea8d95c0913cb30b3ed3a07e9106b6
Alttz <antton.selkanen@gmail.com>
2024-03-18 13:36:10 +0200

    Removed PurchaseRow

--- ticketguru/src/main/java/copypaste/ticketguru/domain/PurchaseRowRepository.java
+++
@@ -1,9 +0,0 @@
-package copypaste.ticketguru.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseRowRepository extends CrudRepository<PurchaseRow, Long> {
-
-}
