4152b54b833d16cb81cc38bf0179e988a8c9851e
Alttz <antton.selkanen@gmail.com>
2024-03-24 16:00:53 +0200

    Updated login and token validation
    Updated code so login works in front-end, while simultaneously JWT token authorization works in the API interface.
    Also added token validation to all api/events/ endpoints.

--- ticketguru/src/main/java/copypaste/ticketguru/securingweb/WebSecurityConfig.java
+++ ticketguru/src/main/java/copypaste/ticketguru/securingweb/WebSecurityConfig.java
@@ -1,11 +1,9 @@
 package copypaste.ticketguru.securingweb;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,17 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
-
 	@Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers("/**").permitAll()
+                                .requestMatchers("/api/**").permitAll()
                                 .anyRequest().authenticated()
                 )
+                
+                .formLogin(form -> form
+                        .loginPage("/login").permitAll()
+                    )
 
                 .logout(logout -> logout.permitAll());
         return http.build();
--- ticketguru/src/main/java/copypaste/ticketguru/web/rest/EventRestController.java
+++ ticketguru/src/main/java/copypaste/ticketguru/web/rest/EventRestController.java
@@ -48,7 +48,7 @@ public class EventRestController {
 		if (authHeader != null && authHeader.startsWith("Bearer ")) {
 			String token = authHeader.substring(7); // Extract the token from the header
 			// Validate the token
-            return jwtUtil.validateToken(token);
+			return jwtUtil.validateToken(token);
 		}
 		return false;
 	}
@@ -56,7 +56,7 @@ public class EventRestController {
 	// hae kaikki tapahtumat
 	@GetMapping(value = "/api/events")
 	public ResponseEntity<?> getAllEvents(@RequestHeader(value = "Authorization", required = false) String authHeader) {
-		if(RestValidateJWT(authHeader)) {
+		if (RestValidateJWT(authHeader)) {
 			List<Event> events = (List<Event>) eventRepository.findAll();
 			return ResponseEntity.ok(events); // Return the events if token is valid
 		}
@@ -77,17 +77,12 @@ public class EventRestController {
 	@GetMapping(value = "/api/events/{id}")
 	public ResponseEntity<?> getEventById(@PathVariable Long id,
 			@RequestHeader(value = "Authorization", required = false) String authHeader) {
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			String token = authHeader.substring(7); // Extract the token from the header
-
-			// Validate the token
-			if (jwtUtil.validateToken(token)) {
-				Optional<Event> eventOpt = eventRepository.findById(id);
-				if (!eventOpt.isPresent()) {
-					return ResponseEntity.notFound().build();
-				}
-				return ResponseEntity.ok(eventOpt.get());
+		if (RestValidateJWT(authHeader)) {
+			Optional<Event> eventOpt = eventRepository.findById(id);
+			if (!eventOpt.isPresent()) {
+				return ResponseEntity.notFound().build();
 			}
+			return ResponseEntity.ok(eventOpt.get());
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
 	}
@@ -106,20 +101,14 @@ public class EventRestController {
 	@GetMapping(value = "/api/events/byName")
 	public ResponseEntity<?> findEventsByName(@RequestParam("name") String name,
 			@RequestHeader(value = "Authorization", required = false) String authHeader) {
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			String token = authHeader.substring(7); // Extract the token from the header
-
-			// Validate the token
-			if (jwtUtil.validateToken(token)) {
-				List<Event> events = eventRepository.findByNameContainingIgnoreCase(name);
-				if (events.isEmpty()) {
-					return ResponseEntity.noContent().build();
-				}
-				return ResponseEntity.ok(events);
+		if (RestValidateJWT(authHeader)) {
+			List<Event> events = eventRepository.findByNameContainingIgnoreCase(name);
+			if (events.isEmpty()) {
+				return ResponseEntity.noContent().build();
 			}
+			return ResponseEntity.ok(events);
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
-
 	}
 
 	/*
@@ -135,20 +124,14 @@ public class EventRestController {
 	@GetMapping(value = "/api/events/byCity")
 	public ResponseEntity<?> findEventsByCity(@RequestParam("city") String city,
 			@RequestHeader(value = "Authorization", required = false) String authHeader) {
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			String token = authHeader.substring(7); // Extract the token from the header
-
-			// Validate the token
-			if (jwtUtil.validateToken(token)) {
-				List<Event> events = eventRepository.findByCityIgnoreCase(city);
-				if (events.isEmpty()) {
-					return ResponseEntity.noContent().build();
-				}
-				return ResponseEntity.ok(events);
+		if (RestValidateJWT(authHeader)) {
+			List<Event> events = eventRepository.findByCityIgnoreCase(city);
+			if (events.isEmpty()) {
+				return ResponseEntity.noContent().build();
 			}
+			return ResponseEntity.ok(events);
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
-
 	}
 
 	/*
@@ -177,119 +160,130 @@ public class EventRestController {
 	@PostMapping(value = "/api/events")
 	public ResponseEntity<?> createEvent(@Valid @RequestBody Event newEvent,
 			@RequestHeader(value = "Authorization", required = false) String authHeader) {
-		if (authHeader != null && authHeader.startsWith("Bearer ")) {
-			String token = authHeader.substring(7); // Extract the token from the header
-
-			// Validate the token
-			if (jwtUtil.validateToken(token)) {
-
-				return ResponseEntity.status(HttpStatus.CREATED).body(eventRepository.save(newEvent));
-			}
+		if (RestValidateJWT(authHeader)) {
+			return ResponseEntity.status(HttpStatus.CREATED).body(eventRepository.save(newEvent));
 		}
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
-
 	}
 
 	// Tapahtuman päivitys
 	@PutMapping(value = "/api/events/{id}")
-	public ResponseEntity<?> updateEvent(@PathVariable Long id, @Valid @RequestBody EventRequest eventRequest) {
-		return eventRepository.findById(id).map(event -> {
-			event.setName(eventRequest.getName());
-			event.setDate(eventRequest.getDate());
-			event.setPlace(eventRequest.getPlace());
-			event.setCity(eventRequest.getCity());
-			event.setTicketCount(eventRequest.getTicketCount());
+	public ResponseEntity<?> updateEvent(@PathVariable Long id, @Valid @RequestBody EventRequest eventRequest,
+			@RequestHeader(value = "Authorization", required = false) String authHeader) {
+		if (RestValidateJWT(authHeader)) {
+			return eventRepository.findById(id).map(event -> {
+				event.setName(eventRequest.getName());
+				event.setDate(eventRequest.getDate());
+				event.setPlace(eventRequest.getPlace());
+				event.setCity(eventRequest.getCity());
+				event.setTicketCount(eventRequest.getTicketCount());
 
-			Event updatedEvent = eventRepository.save(event);
-			return ResponseEntity.ok(updatedEvent);
-		}).orElse(ResponseEntity.notFound().build());
+				Event updatedEvent = eventRepository.save(event);
+				return ResponseEntity.ok(updatedEvent);
+			}).orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
 	}
 
 	// Tapahtuman poisto
 	@DeleteMapping("/api/delete/{id}")
-	public ResponseEntity<?> deleteEvent(@PathVariable long id) {
-		return eventRepository.findById(id).map(event -> {
-			eventRepository.delete(event);
-			return ResponseEntity.noContent().build();
-		}).orElse(ResponseEntity.notFound().build());
+	public ResponseEntity<?> deleteEvent(@PathVariable long id,
+			@RequestHeader(value = "Authorization", required = false) String authHeader) {
+		if (RestValidateJWT(authHeader)) {
+			return eventRepository.findById(id).map(event -> {
+				eventRepository.delete(event);
+				return ResponseEntity.noContent().build();
+			}).orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
 	}
 
 	// Lisää lipputyypit tapahtumaan
 	@PostMapping("/api/events/{eventId}/tickettypes")
 	public ResponseEntity<?> createTicketTypesForEvent(@PathVariable Long eventId,
-			@Valid @RequestBody List<TicketTypeRequest> ticketTypeRequests) {
-		// Tarkastetaan löytyykö tapahtuma id:n perusteella
-		return eventRepository.findById(eventId).map(event -> {
-			List<TicketType> ticketTypes = new ArrayList<>();
-			for (TicketTypeRequest ttRequest : ticketTypeRequests) {
-				if (!ALLOWED_TICKET_TYPES.contains(ttRequest.getName())) {
-					return ResponseEntity.badRequest().body("Ticket type " + ttRequest.getName() + " is not allowed.");
+			@Valid @RequestBody List<TicketTypeRequest> ticketTypeRequests,
+			@RequestHeader(value = "Authorization", required = false) String authHeader) {
+		if (RestValidateJWT(authHeader)) {
+			// Tarkastetaan löytyykö tapahtuma id:n perusteella
+			return eventRepository.findById(eventId).map(event -> {
+				List<TicketType> ticketTypes = new ArrayList<>();
+				for (TicketTypeRequest ttRequest : ticketTypeRequests) {
+					if (!ALLOWED_TICKET_TYPES.contains(ttRequest.getName())) {
+						return ResponseEntity.badRequest()
+								.body("Ticket type " + ttRequest.getName() + " is not allowed.");
+					}
+					TicketType newTicketType = new TicketType(ttRequest.getName(), ttRequest.getPrice(), event);
+					ticketTypes.add(newTicketType);
 				}
-				TicketType newTicketType = new TicketType(ttRequest.getName(), ttRequest.getPrice(), event);
-				ticketTypes.add(newTicketType);
-			}
 
-			List<TicketType> savedTicketTypes = (List<TicketType>) ticketTypeRepository.saveAll(ticketTypes);
+				List<TicketType> savedTicketTypes = (List<TicketType>) ticketTypeRepository.saveAll(ticketTypes);
 
-			// Yksinkertaistetaan JSON-vastausta
-			List<Map<String, Object>> response = savedTicketTypes.stream().map(tt -> {
-				Map<String, Object> ticketTypeResponse = new HashMap<>();
-				ticketTypeResponse.put("id", tt.getId());
-				ticketTypeResponse.put("name", tt.getName());
-				ticketTypeResponse.put("price", tt.getPrice());
-				ticketTypeResponse.put("event", tt.getEvent().getId()); // sisällytä vain ID
-				return ticketTypeResponse;
-			}).collect(Collectors.toList());
+				// Yksinkertaistetaan JSON-vastausta
+				List<Map<String, Object>> response = savedTicketTypes.stream().map(tt -> {
+					Map<String, Object> ticketTypeResponse = new HashMap<>();
+					ticketTypeResponse.put("id", tt.getId());
+					ticketTypeResponse.put("name", tt.getName());
+					ticketTypeResponse.put("price", tt.getPrice());
+					ticketTypeResponse.put("event", tt.getEvent().getId()); // sisällytä vain ID
+					return ticketTypeResponse;
+				}).collect(Collectors.toList());
 
-			return ResponseEntity.status(HttpStatus.CREATED).body(response);
-		}).orElse(ResponseEntity.notFound().build());
+				return ResponseEntity.status(HttpStatus.CREATED).body(response);
+			}).orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
 	}
 
 	// Päivität tapahtuman lipputyyppejä
 	@PutMapping("/api/events/{eventId}/tickettypes")
 	public ResponseEntity<?> updateTicketTypesForEvent(@PathVariable Long eventId,
-			@RequestBody List<TicketTypeRequest> ticketTypeRequests) {
-		// Tarkastetaan löytyykö tapahtuma id:n perusteella
-		return eventRepository.findById(eventId).map(event -> {
-			List<TicketType> existingTicketTypes = ticketTypeRepository.findByEvent(event);
+			@RequestBody List<TicketTypeRequest> ticketTypeRequests,
+			@RequestHeader(value = "Authorization", required = false) String authHeader) {
+		if (RestValidateJWT(authHeader)) {
+			// Tarkastetaan löytyykö tapahtuma id:n perusteella
+			return eventRepository.findById(eventId).map(event -> {
+				List<TicketType> existingTicketTypes = ticketTypeRepository.findByEvent(event);
 
-			// Tarkasta onko lipputyypeille jo luotu lippuja
-			boolean areTicketTypesUsed = existingTicketTypes.stream()
-					.anyMatch(tt -> !ticketRepository.findByTicketType(tt).isEmpty());
+				// Tarkasta onko lipputyypeille jo luotu lippuja
+				boolean areTicketTypesUsed = existingTicketTypes.stream()
+						.anyMatch(tt -> !ticketRepository.findByTicketType(tt).isEmpty());
 
-			if (areTicketTypesUsed) {
-				return ResponseEntity.badRequest()
-						.body("Cannot update ticket types as tickets have already been created.");
-			}
-
-			// Poistetaan aikaisemmat lipputyypit
-			ticketTypeRepository.deleteAll(existingTicketTypes);
-
-			// Tarkastetaan lipputyyppien nimet, luodaan uudet lipputyypit ja määritetään
-			// mihin tapahtumaan ne kuuluvat
-			List<TicketType> ticketTypes = new ArrayList<>();
-			for (TicketTypeRequest ttRequest : ticketTypeRequests) {
-				if (!ALLOWED_TICKET_TYPES.contains(ttRequest.getName())) {
-					return ResponseEntity.badRequest().body("Ticket type " + ttRequest.getName() + " is not allowed.");
+				if (areTicketTypesUsed) {
+					return ResponseEntity.badRequest()
+							.body("Cannot update ticket types as tickets have already been created.");
 				}
-				TicketType newTicketType = new TicketType(ttRequest.getName(), ttRequest.getPrice(), event);
-				ticketTypes.add(newTicketType);
-			}
 
-			List<TicketType> savedTicketTypes = (List<TicketType>) ticketTypeRepository.saveAll(ticketTypes);
+				// Poistetaan aikaisemmat lipputyypit
+				ticketTypeRepository.deleteAll(existingTicketTypes);
 
-			// Yksinkertaistetaan JSON-vastausta
-			List<Map<String, Object>> response = savedTicketTypes.stream().map(tt -> {
-				Map<String, Object> ticketTypeResponse = new HashMap<>();
-				ticketTypeResponse.put("id", tt.getId());
-				ticketTypeResponse.put("name", tt.getName());
-				ticketTypeResponse.put("price", tt.getPrice());
-				ticketTypeResponse.put("event", tt.getEvent().getId()); // Sisällytetään vastaukseen vain tapahtuman id
-				return ticketTypeResponse;
-			}).collect(Collectors.toList());
+				// Tarkastetaan lipputyyppien nimet, luodaan uudet lipputyypit ja määritetään
+				// mihin tapahtumaan ne kuuluvat
+				List<TicketType> ticketTypes = new ArrayList<>();
+				for (TicketTypeRequest ttRequest : ticketTypeRequests) {
+					if (!ALLOWED_TICKET_TYPES.contains(ttRequest.getName())) {
+						return ResponseEntity.badRequest()
+								.body("Ticket type " + ttRequest.getName() + " is not allowed.");
+					}
+					TicketType newTicketType = new TicketType(ttRequest.getName(), ttRequest.getPrice(), event);
+					ticketTypes.add(newTicketType);
+				}
 
-			return ResponseEntity.ok(response);
-		}).orElse(ResponseEntity.notFound().build());
+				List<TicketType> savedTicketTypes = (List<TicketType>) ticketTypeRepository.saveAll(ticketTypes);
+
+				// Yksinkertaistetaan JSON-vastausta
+				List<Map<String, Object>> response = savedTicketTypes.stream().map(tt -> {
+					Map<String, Object> ticketTypeResponse = new HashMap<>();
+					ticketTypeResponse.put("id", tt.getId());
+					ticketTypeResponse.put("name", tt.getName());
+					ticketTypeResponse.put("price", tt.getPrice());
+					ticketTypeResponse.put("event", tt.getEvent().getId()); // Sisällytetään vastaukseen vain tapahtuman
+																			// id
+					return ticketTypeResponse;
+				}).collect(Collectors.toList());
+
+				return ResponseEntity.ok(response);
+			}).orElse(ResponseEntity.notFound().build());
+		}
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or missing token");
 	}
-
 }
