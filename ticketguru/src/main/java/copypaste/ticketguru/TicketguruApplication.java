86498d5d7aa12ff1ac549881be1dab6c3e89233e
Alttz <antton.selkanen@gmail.com>
2024-04-09 17:30:36 +0300

    Updated code to enable merging to main and building to OpenShift

--- ticketguru/src/main/java/copypaste/ticketguru/TicketguruApplication.java
+++ ticketguru/src/main/java/copypaste/ticketguru/TicketguruApplication.java
@@ -24,15 +24,13 @@ import copypaste.ticketguru.domain.UserRepository;
 @SpringBootApplication
 public class TicketguruApplication {
 	
-	static {
-        System.setProperty("spring.datasource.username", System.getenv("DB_USER"));
-        System.setProperty("spring.datasource.password", System.getenv("DATABASE_PASSWORD")); 
-        System.setProperty("spring.datasource.service_host", System.getenv("DB_SERVICE_SERVICE_HOST"));
-        System.setProperty("spring.datasource.service_port", System.getenv("DB_SERVICE_SERVICE_PORT")); 
-        System.setProperty("spring.datasource.name", System.getenv("DB_PASSWORD")); 
+	/* static {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+        System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USER"));
+        System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
 
-
-    } 
+    } */
 
 	public static void main(String[] args) {
 
